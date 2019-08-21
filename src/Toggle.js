d7ed71b79b57b02fa3e507e05a7b1a3a63acdfbc
Rafael Pereira <rafaelszp@users.noreply.github.com>
2019-08-21 15:01:18 -0300

    toggle!
    now toggle checkboxes reflexes the state.ui.toggle property

--- src/Toggle.js
+++ src/Toggle.js
@@ -5,22 +5,18 @@ import { TOGGLE, toggleSwitch } from "./UiReducer";
 const Toggle = () => {
   const ui = useSelector(state => state.ui);
   const dispatch = useDispatch();
-  const toggle = () =>  toggleSwitch()(dispatch);
+  const toggle = () => toggleSwitch()(dispatch);
   return (
     <div>
       <div>{JSON.stringify(ui)}</div>
       <p>Method 1</p>
       <input
         type="checkbox"
-        value={ui.toggle}
+        checked={ui.toggle}
         onChange={() => dispatch({ type: TOGGLE })}
       />
       <p>Method 2</p>
-      <input
-        type="checkbox"
-        value={ui.toggle}
-        onChange={() => toggle()}
-      />
+      <input type="checkbox" checked={ui.toggle} onChange={() => toggle()} />
     </div>
   );
 };
